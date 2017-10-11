63a2ab3acd65e9e55122b3ddcad1890db8d843f6
ginoy <GinoyJda>
2017-10-11 13:51:00 +0800

    add startbanner

---
+++ src/main/java/neusoft/test/D.java
@@ -0,0 +1,11 @@
+package neusoft.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 17-10-11
+ * Time: 下午1:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class D {
+}
